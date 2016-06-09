f5643144ed04550a4e0a6281e47f2d32eec37e9c
shivasdst <shivably04sdst@gmail.com>
2016-06-09 17:03:56 +0530

    No. 3 - changed configurations

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASE_URL', 'http://localhost/iitm/');
+define('BASE_URL', 'http://localhost/tact/');
 define('PUBLIC_URL', BASE_URL . 'public/');
 define('XML_SRC_URL', BASE_URL . 'md-src/xml/');
 define('PHOTO_URL', PUBLIC_URL . 'Photos/');
@@ -9,7 +9,7 @@ define('STOCK_IMAGE_URL', PUBLIC_URL . 'images/stock/');
 define('RESOURCES_URL', PUBLIC_URL . 'Resources/');
 
 // Physical location of resources
-define('PHY_BASE_URL', '/var/www/html/iitm/');
+define('PHY_BASE_URL', '/var/www/html/tact/');
 define('PHY_PUBLIC_URL', PHY_BASE_URL . 'public/');
 define('PHY_XML_SRC_URL', PHY_BASE_URL . 'md-src/xml/');
 define('PHY_PHOTO_URL', PHY_PUBLIC_URL . 'Photos/');
@@ -19,13 +19,13 @@ define('PHY_FLAT_URL', PHY_BASE_URL . 'application/views/flat/');
 define('PHY_STOCK_IMAGE_URL', PHY_PUBLIC_URL . 'images/stock/');
 define('PHY_RESOURCES_URL', PHY_PUBLIC_URL . 'Resources/');
 
-define('DB_PREFIX', 'iitm');
+define('DB_PREFIX', 'tact');
 define('DB_HOST', 'localhost');
 
 // photo will become iitmPHOTO inside
-define('DB_NAME', 'heritage');
+define('DB_NAME', 'general');
 
-define('heritage_USER', 'root');
-define('heritage_PASSWORD', 'mysql');
+define('general_USER', 'root');
+define('general_PASSWORD', 'mysql');
 
 ?>
